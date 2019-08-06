513ac921a7d6a11d8995be5dc22a212f81fa09a0
Nicholas Springer <nicholas.m.springer@gmail.com>
2019-08-06 13:56:56 -0700

    edit accordion button style for website

--- docs/static/accordion.js
+++ docs/static/accordion.js
@@ -4,13 +4,18 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         var panel = this.nextElementSibling;
-        var prevActive = this.previousElementSibling.previousElementSibling.classList.contains("active") || this.textContent=="Hint 1";
-        if (panel.style.display === "block") {
-            this.classList.toggle("active");
+        if(this.classList.contains("active")) {
+            this.classList.remove("active");
             panel.style.display = "none";
-        } else if(prevActive){
-            this.classList.toggle("active");
-            panel.style.display = "block";
+        } else {
+            var allowOpen = this.previousElementSibling.previousElementSibling.classList.contains("prevActive") || this.textContent=="Hint 1";
+            if(allowOpen) {
+                if(!this.classList.contains("prevActive")){
+                    this.classList.add("prevActive");
+                }
+                this.classList.add("active");
+                panel.style.display = "block";
+            } 
         }
-    });
+    })
 }
