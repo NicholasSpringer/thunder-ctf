3eab6e4a076d962b332961a2d2e5fef2d475c516
Nicholas Springer <nicholas.m.springer@gmail.com>
2019-08-12 09:38:05 -0700

    fixed whitespace in html

--- hint-templating/generate_level_docs.py
+++ hint-templating/generate_level_docs.py
@@ -2,15 +2,19 @@ import os
 from jinja2 import Template
 
 os.chdir(os.getcwd()+'/'+os.path.dirname(__file__))
-with open('level-template.jinja') as f:
+with open('level-hints-template.jinja') as f:
     template = Template(f.read())
 for file_name in os.listdir('level-hints'):
     with open(f'level-hints/{file_name}') as f:
+        # Split hints in file
         blocks = f.read().split('\n---\n')
     level_name = file_name.split('.')[0]
+    intro = blocks[0].replace('\n<',f'\n{" "*8}<')
+    hints = [block.replace('\n<',f'\n{" "*10}<') for block in blocks[1:]]
     jinja_args = {'level_name': level_name,
-                  'intro': blocks[0],
-                  'hints': blocks[1:]}
+                  'intro': intro,
+                  'hints': hints}
+    
     render = template.render(**jinja_args)
     with open(f'{os.path.dirname(os.getcwd())}/docs/levels/{level_name}.html', 'w+') as f:
         f.write(render)
