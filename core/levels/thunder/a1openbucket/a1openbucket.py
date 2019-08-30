a70574846a4aa6b53247c7360aeecbcc09bf8035
Nicholas Springer <nicholas.m.springer@gmail.com>
2019-08-30 11:02:15 -0700

    edit a1 openbucket to add template comments

--- core/levels/thunder/a1openbucket/a1openbucket.py
+++ core/levels/thunder/a1openbucket/a1openbucket.py
@@ -10,16 +10,22 @@ RESOURCE_PREFIX = 'a1'
 
 
 def create():
+    # ---------Level Initialization---------
     # Create randomized bucket name to avoid namespace conflict
     nonce = str(random.randint(100000000000, 999999999999))
     bucket_name = f'{RESOURCE_PREFIX}-bucket-{nonce}'
+    # --------------------------------------
+
+    # ---------Deployment Insertion---------
     # Insert deployment
     config_template_args = {'nonce': nonce}
     template_files = ['core/framework/templates/bucket_acl.jinja']
     deployments.insert(LEVEL_PATH,
                        template_files=template_files,
                        config_template_args=config_template_args)
+    # --------------------------------------
 
+    # --------------Level Setup-------------
     print("Level setup started for: " + LEVEL_PATH)
     # Insert secret into bucket
     storage_client = storage.Client()
@@ -27,10 +33,13 @@ def create():
     secret_blob = storage.Blob('secret.txt', bucket)
     secret = levels.make_secret(LEVEL_PATH)
     secret_blob.upload_from_string(secret)
+    
+    # Print complete message and print/save start info
     print(f'Level creation complete for: {LEVEL_PATH}\n'
           f'Instruction for the level can be accessed at thunder-ctf.cloud/levels/{LEVEL_PATH}.html')
     start_message = f'The secret for this level can be found in the Google Cloud Storage (GCS) bucket {bucket_name}'
     levels.write_start_info(LEVEL_PATH, start_message)
+    # --------------------------------------
 
 
 def destroy():
