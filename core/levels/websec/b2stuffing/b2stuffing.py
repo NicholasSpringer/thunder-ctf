480c6adba55c5fee7303c4dcf6611e4c94ecd7e2
Wenjing Wu <wwu@pdx.edu>
2019-12-19 22:32:06 -0800

    dynamically create b2 yaml and some clean up

--- core/levels/websec/b2stuffing/b2stuffing.py
+++ core/levels/websec/b2stuffing/b2stuffing.py
@@ -11,7 +11,10 @@ from core.framework.cloudhelpers import deployments, iam, gcstorage, cloudfuncti
 LEVEL_PATH = 'websec/b2stuffing'
 RESOURCE_PREFIX = 'b2'
 INSTANCE_ZONE = 'us-west1-b'
-
+LEVEL_NAME ='b2stuffing'
+CRED_PATH='scripts/'+RESOURCE_PREFIX+'/'
+NUM_OF_CONTAINERS = 3
+SOURCE = 'core/levels/'+LEVEL_PATH+'/'
 
 def create():
     print("Level initialization started for: " + LEVEL_PATH)
@@ -19,69 +22,61 @@ def create():
     nonce = str(random.randint(100000000000, 999999999999))
     bucket_name = f'{RESOURCE_PREFIX}-bucket-{nonce}'
     print("Level initialization finished for: " + LEVEL_PATH)
+       
     
-    source = 'core/levels/'+LEVEL_PATH+'/'
-    dest='scripts/'+RESOURCE_PREFIX+'/'
-    user_r, pass_r, CREDS= gen_credentials(source,dest,10,3)
+    vcreds, RCREDS= gen_credentials(SOURCE,CRED_PATH,10,NUM_OF_CONTAINERS)
     
+    #Prepare args and yaml
+    container_yaml = SOURCE+'container.yaml'  
+    conyaml = open(container_yaml,'r')
+    container = conyaml.read()
+    conyaml.close()
+    
+    attack_yaml = SOURCE+'attack.yaml'
+    attyaml = open(attack_yaml,'r')
+    attack = attyaml.read()
+    attyaml.close()
+    
+    level_yaml = SOURCE+LEVEL_NAME+'.yaml'
+    levyaml = open(level_yaml,'a')
+    levyaml.write(attack)   
 
-    # Insert deployment
     config_template_args = {'nonce': nonce}
-    for i in range(len(user_r)):
+    
+    for i in range(NUM_OF_CONTAINERS):
         kn='user_r'+str(i)
-        vn=user_r[i]
+        vn=list(vcreds[i].keys())[0]
         kp='pass_r'+str(i)
-        vp=pass_r[i]
+        vp=list(vcreds[i].values())[0]
         config_template_args[kn]=vn
         config_template_args[kp]=vp
-
+        levyaml.write(container.replace('{UUU}',kn).replace('{PPP}',kp).replace('{INDEX}',str(i)))
+    levyaml.close()
+    
+    # Insert deployment
     template_files = [
         'core/framework/templates/container_vm.jinja','core/framework/templates/ubuntu_vm.jinja']
     deployments.insert(LEVEL_PATH, template_files=template_files,
                        config_template_args=config_template_args)
 
     print("Level setup started for: " + LEVEL_PATH)
-    # Insert secret into bucket
-    #storage_client = storage.Client()
-    #bucket = storage_client.get_bucket(bucket_name)
-    #secret_blob = storage.Blob('secret.txt', bucket)
-    #secret = levels.make_secret(LEVEL_PATH)
-    #secret_blob.upload_from_string(secret)
-
-    # Create service account key file
-    #sa_key = iam.generate_service_account_key(f'{RESOURCE_PREFIX}-access')
 
     print(f'Level creation complete for: {LEVEL_PATH}')
     start_message = (
-        f'Use attack.py and credentials.py to find valid credential of vulnerable websites. \nProssible credentials are {CREDS}.')
+        f'Use attack.py and credentials.py to find valid credential of vulnerable websites. \nProssible credentials are {RCREDS}.')
     levels.write_start_info(LEVEL_PATH, start_message)
     print(
         f'Instruction for the level can be accessed at thunder-ctf.cloud/levels/{LEVEL_PATH}.html')
-    
-    #move helper scripts to thunder-ctf/start
-    #will fail if dicrectory start/ not being created in previous steps--examples levels.write_start_info
-    #to be continue....
-    
-    #dest = 'start/'
-    #for f in os.listdir(source):
-    #    cmd='cp '+ source+f +' '+ dest+f
-    #    os.popen(cmd)
-        #os.replace(source+f, dest+f)
-    #change permission
-    #for f in os.listdir(dest):
-    #    os.chmod(dest+f, 0o700)
-    #remove empty helper directory
-    #os.rmdir(source)
 
 def gen_credentials(source,dest,n,m):
     
-    #possible usernames
+    #Possible usernames
     u_srouce=source+'unames.txt'
     names=[uname.rstrip('\n') for uname in open(u_srouce,'r')]
-    #most commanly used passwords
+    #Most commanly used passwords
     p_srouce=source+'passwords.txt'
     passwords=[password.rstrip('\n').strip() for password in open(p_srouce,'r')]
-    #randomly generate 10 credientials and write to file
+    #Randomly generate 10 credientials and write to file
     rcreds={}
     random.shuffle(names)
     random.shuffle(passwords)
@@ -91,20 +86,23 @@ def gen_credentials(source,dest,n,m):
     f = open(c_dest, "w")
     f.write("creds = "+str(rcreds))
     f.close()
-    #randomly generate one valid credientials
+    #Randomly generate one valid credientials
     index=random.randint(0,n-1)
-    vnames=[list(rcreds.keys())[index]]
-    vpass=[list(rcreds.values())[index]]
-    #generate m-1 valid credientials
-    for i in range(m-1):
-        vnames.append(names[n+i])
-        vpass.append(passwords[n+i]) 
-    random.shuffle(vnames)
-    random.shuffle(vpass)     
-    return vnames,vpass,rcreds
+    vcreds=[{list(rcreds.keys())[index] : list(rcreds.values())[index]}]
+    #Generate m-1 valid credientials
+    for i in range(m-1): 
+        vcreds.append({names[n+i]:passwords[n+i]}) 
+    random.shuffle(vcreds)     
+    return vcreds,rcreds
 
 def destroy():
     # Delete starting files
     levels.delete_start_files()
+    # Delete credentials.py
+    if os.path.exists(CRED_PATH+'credentials.py'):
+        os.remove(CRED_PATH+'credentials.py')
+    # Empty level yaml file
+    level_yaml = SOURCE+LEVEL_NAME+'.yaml'
+    open(level_yaml, 'w').close()
     # Delete deployment
     deployments.delete()
