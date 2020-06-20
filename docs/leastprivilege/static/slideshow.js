08231809a22a20b15517d69211ca1c8784d9ae1e
Wenjing Wu <wwu@pdx.edu>
2020-06-19 20:10:48 -0700

    ui update

---
+++ docs/leastprivilege/static/slideshow.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    var showWriteup = false;
+    var pos = 0;
+    var max = $("#hint_content").children().length-1;
+    function switchHint() {
+        if(pos-1==0) $("#prev").animate({ opacity: 100 }).removeClass("buttondisabled");
+        if(pos+1==max) $("#next").animate({ opacity: 100 }).removeClass("buttondisabled");
+        if(pos==0) $("#prev").animate({ opacity: 0 }).addClass("buttondisabled");
+        if(pos==max) $("#next").animate({ opacity: 0 }).addClass("buttondisabled");
+        $("#hint_display").hide().html($("#hint_content").children().eq(pos).children().slice(1).clone()).fadeIn("fast");
+        $("#hint_title").hide().html($("#hint_content").children().eq(pos).children().eq(0).html()).fadeIn("fast");
+    }
+
+    function toggleWriteup() {
+        showWriteup = !showWriteup;
+        if(!showWriteup) {
+            if(!(pos==0)) $("#prev").animate({ opacity: 100 }).removeClass("buttondisabled");
+            if(!(pos==max)) $("#next").animate({ opacity: 100 }).removeClass("buttondisabled");
+            switchHint();
+            $("#writeup_button").hide().html("Show Level Writeup").fadeIn("fast");
+        } else {
+            $("#prev").animate({ opacity: 0 }).addClass("buttondisabled")
+            $("#next").animate({ opacity: 0 }).addClass("buttondisabled")
+            $("#hint_display").hide().html($("#writeup_content").html()).fadeIn("fast");
+            $("#hint_title").hide().html("Level Writeup").fadeIn("fast");
+            $("#writeup_button").hide().html("Show Level Instructions").fadeIn("fast");
+        }
+    }
+
+    switchHint(pos)
+    $('#prev').on('click', function () {
+        if (pos > 0) {
+            pos--;
+            switchHint();
+        }
+    });
+
+    $('#next').on('click', function () {
+        if (pos < $("#hint_content").children().length - 1) {
+            pos++;
+            switchHint();
+        } 
+        
+    });
+    
+    $('#writeup_button').on('click', function () {
+        toggleWriteup();
+    });
+
+    $('.toggle_thunder').on('click', function () {
+        $("#about_thunder").toggle(50);
+    });
+
+
+    $('.toggle_lst').on('click', function () {
+        $("#about_lst").toggle(50);
+    });
+})
