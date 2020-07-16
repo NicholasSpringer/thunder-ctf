c56df809882b7d465af570ce130c7946754e067a
Wenjing Wu <wwu@pdx.edu>
2020-07-16 05:58:30 -0700

    remove patch add second deploy for insert

--- core/framework/cloudhelpers/deployments.py
+++ core/framework/cloudhelpers/deployments.py
@@ -46,7 +46,7 @@ def _read_render_config(file_name, template_args={}, load_path=[]):
 
 
 def insert(level_path, template_files=[],
-           config_template_args={}, labels={}):
+           config_template_args={}, labels={}, second_deploy = False):
     '''Inserts a deployment using deployment manager, importing any specified template files. 
         If template arguments are included, the top level configuration file will be rendered using Jinja2.
 
@@ -61,6 +61,7 @@ def insert(level_path, template_files=[],
             and can be retrieved later using `framework.cloudhelpers.deployments.get_labels`.
             Labels are the recommended way to store any information that will be necessary for level deletion.
             The keyword "level" is reserved for storing the active level path.
+        second_deploy (boolean): Automatically start destroy level and recreate if set to True
     '''
     # Get current credentials from environment variables and build deployment API object
     credentials, project_id = google.auth.default()
@@ -109,86 +110,17 @@ def insert(level_path, template_files=[],
     operation = deployment_api.deployments().insert(
         project=project_id, body=request_body).execute()
     op_name = operation['name']
-    _wait_for_operation(op_name, deployment_api,
-                        project_id, level_path=level_path)
-
-def patch(level_path, template_files=[],
-           config_template_args={}, labels={}, second_deploy = False):
-    '''Patches a deployment using deployment manager, importing any specified template files. 
-        If template arguments are included, the top level configuration file will be rendered using Jinja2.
-
-    Parameters:
-        level_path (str): Relative path of the level from the levels/ directory
-        template_files (list of str, optional): List of paths of the template files that are used in the deployment configuration, starting with "core/".
-            The names of the templates in the configuration use the filenames of the templates, not the full paths.
-        config_template_args (dict, optional): Dictionary of arguments to use when rendering the top level configuration template using Jinja2.
-            Keys should be strings that correspond to the names of variables in the Jinja template, and each corresponding value should be the passed value of the variable.
-            If not supplied, the top level configuration will not be treated as a template.
-        labels (dict, optional): Dictionary of key/value pairs that will be included as labels on the deployment, 
-            and can be retrieved later using `framework.cloudhelpers.deployments.get_labels`.
-            Labels are the recommended way to store any information that will be necessary for level deletion.
-            The keyword "level" is reserved for storing the active level path.
-        second_deploy (boolean): Automatically start destroy level and recreate if set to True
-    '''
-    # Get current credentials from environment variables and build deployment API object
-    credentials, project_id = google.auth.default()
-    deployment_api = discovery.build(
-        'deploymentmanager', 'v2', credentials=credentials)
     
-    # get current deployment
-    current_depoy = deployment_api.deployments().get(
-        project=project_id, deployment='thunder').execute()
-    labels = current_depoy['labels']
-    fingerprint = current_depoy['fingerprint']
-    level_name = os.path.basename(level_path)
-    
-    #render patched contnent
-    content_patch = _read_render_config(
-                    f'{level_name}_patch.yaml',
-                    template_args=config_template_args,
-                    load_path =  f'core/levels/{level_path}/'
-                )
-    
-    # Create request to patch deployment
-    request_body = {
-        "name": "thunder",
-        "fingerprint": fingerprint,
-        "target": {
-            "config": {
-                "content": content_patch
-            },
-            "imports": []
-        },
-        "labels": [labels]
-    }
-    # Add imports to deployment json
-    for template in template_files:
-        request_body['target']['imports'].append(
-            {"name": os.path.basename(template),
-             "content": _read_render_config(template)})
-        # If schema is present in sibling directory to template, import it
-        schema_path = f'{os.path.dirname(template)}/schema/{os.path.basename(template)}.schema'
-        if os.path.exists(schema_path):
-            request_body['target']['imports'].append(
-                {"name": os.path.basename(template) + '.schema',
-                 "content": _read_render_config(schema_path)})
-    
-    # Send patch request then wait for operation
-    try:
-        operation = deployment_api.deployments().patch(
-            project=project_id, deployment='thunder', body=request_body).execute()
-        op_name = operation['name']
-    except Exception as e: 
-        print(str(e))
-
-    if not second_deploy:
+    if second_deploy:
         #destroy and restart deployment if error in patching operation
-        _wait_for_patch(op_name, deployment_api,
+        _wait_for_operation2(op_name, deployment_api,
                             project_id, level_path=level_path)
     else:
 
         _wait_for_operation(op_name, deployment_api,
                             project_id, level_path=level_path)
+
+
 
 def delete():
     '''Deletes the active deployment. 
@@ -273,9 +205,10 @@ def _wait_for_operation(op_name, deployment_api, project_id, level_path=None):
             level_module.destroy()
             exit()
 
-def _wait_for_patch(op_name, deployment_api, project_id, level_path=None):
+def _wait_for_operation2(op_name, deployment_api, project_id, level_path=None):
     # Wait till  operation finishes, giving updates every 5 seconds
-    #destroy and restart deployment if error in patching operation
+    #destroy and restart deployment if error in deployment operation
+    #This is created because certain types of function deployment failure in new project can be fixed automatically through second deploy
     op_done = False
     t = 0
     start_time = time.time()
@@ -283,7 +216,7 @@ def _wait_for_patch(op_name, deployment_api, project_id, level_path=None):
     while not op_done:
         time_string = f'[{int(t/60)}m {(t%60)//10}{t%10}s]'
         sys.stdout.write(
-            f'\r{time_string}Deployment patching in progress...')
+            f'\r{time_string}Deployment operation in progress...')
         t += 5
         while t < time.time()-start_time:
             t += 5
@@ -293,16 +226,16 @@ def _wait_for_patch(op_name, deployment_api, project_id, level_path=None):
             operation=op_name).execute()['status']
         op_done = (op_status == 'DONE')
     sys.stdout.write(
-        f'\r{time_string}Deployment patching in progress... Done\n')
+        f'\r{time_string}Deployment operation in progress... Done\n')
     operation = op_status = deployment_api.operations().get(
         project=project_id,
         operation=op_name).execute()
     if 'error' in operation and level_path:
-        print("\nDeployment patching Error:\n" + yaml.dump(operation['error']))
+        print("\nDeployment Error:\n" + yaml.dump(operation['error']))
         print("\nSecond try of deploymnent")
         level_module = levels.import_level(level_path)
         level_module.destroy()
-        level_module.create(True)
+        level_module.create(False)
         
     
     
