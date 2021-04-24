3028c762109de3dbd1fb2cc00bfde71580cc8490
Janosch Achenbach <janosch@pdx.edu>
2021-04-24 14:45:01 -0700

    fix

--- core/levels/defender/audit/audit.py
+++ core/levels/defender/audit/audit.py
@@ -2,6 +2,7 @@ import random
 import os
 import json
 import csv
+import sqlalchemy
 
 from google.oauth2 import service_account
 from core.framework import levels
@@ -72,15 +73,6 @@ def destroy():
 def register_users():
     # Load the synthetic user and developer information
     users = csv.DictReader(open('resources/users.csv', newline=''))
-    stmt = "INSERT INTO users (name, phone, address) VALUES "
-    
-    user = line.split(",")
-    if(user[0] == "name"):
-        continue
-    stmt += "(" + user[0] + "," + user[2] + "," + user[1] + ");"
-    stmt = stmt[:-1] + ";"
-    
-
     devs = csv.DictReader(open('resources/devs.csv', newline=''))
 
     # Hit the db api to add users to the table and generate a bunch
@@ -105,38 +97,3 @@ def create_tables(user_db_name):
                 ADDRESS  TEXT              NOT NULL
             );"""
         )
-
-def get_index_context():
-    votes = []
-
-    with db.connect() as conn:
-        # Execute the query and fetch all results
-        recent_votes = conn.execute(
-            "SELECT candidate, time_cast FROM votes "
-            "ORDER BY time_cast DESC LIMIT 5"
-        ).fetchall()
-        # Convert the results into a list of dicts representing votes
-        for row in recent_votes:
-            votes.append({
-                'candidate': row[0],
-                'time_cast': row[1]
-            })
-
-        stmt = sqlalchemy.text(
-            "SELECT COUNT(vote_id) FROM votes WHERE candidate=:candidate")
-        # Count number of votes for tabs
-        tab_result = conn.execute(stmt, candidate="TABS").fetchone()
-        tab_count = tab_result[0]
-        # Count number of votes for spaces
-        space_result = conn.execute(stmt, candidate="SPACES").fetchone()
-        space_count = space_result[0]
-
-    return {
-        'space_count': space_count,
-        'recent_votes': votes,
-        'tab_count': tab_count,
-    }
-
-
-
-
