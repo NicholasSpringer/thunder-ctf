44728b3121bc0f984dc8102b1dabfa308a15012d
Austen Nelson <ajn6@pdx.edu>
2021-04-29 13:26:19 +0000

    added register function

--- core/levels/defender/audit/audit.py
+++ core/levels/defender/audit/audit.py
@@ -1,5 +1,7 @@
 import random
 import os
+import subprocess
+import time
 import json
 import csv
 import sqlalchemy
@@ -23,22 +25,16 @@ def create(second_deploy=True):
     print("Level initialization started for: " + LEVEL_PATH)
     nonce = str(random.randint(100000000000, 999999999999))
 
-    register_func_url = cloudfunctions.upload_cloud_function(
-            'core/levels/defender/audit/resources/register_func',
-            FUNCTION_LOCATION
-            )
-
     config_template_args = {
         'nonce': nonce,
-        'register_url': register_func_url,
         'root_password': 'psw'
         }
 
     template_files = [
-        'core/framework/templates/cloud_function.jinja',
         'core/framework/templates/service_account.jinja',
         'core/framework/templates/iam_policy.jinja',
-        'core/framework/templates/sql_db.jinja'
+        'core/framework/templates/sql_db.jinja',
+        'core/framework/templates/container_vm.jinja'
         ]
 
     if second_deploy:
@@ -54,43 +50,22 @@ def create(second_deploy=True):
             template_files=template_files,
             config_template_args=config_template_args
         )
-    try:
-        print("Level setup started for: " + LEVEL_PATH)
 
-        create_tables()
-        user_keys = register_users()
-        post_statuses()
-
-        print(f'Level creation complete for: {LEVEL_PATH}')
-        start_message = ('Helpful start message')
-
-    except Exception as e:
-        exit()
-
-def destroy():
-    deployments.delete()
-
-def register_users():
-    users = csv.DictReader(open('resources/users.csv', newline=''))
-
-    # Hit the db api to add users to the table and generate a bunch
-    # of account keys might have to wait for the service accounts
-    # to register. Try every 60 seconds
-
-def post_statuses():
-    # Load statuses and post to db
-    statuses = csv.DictReader(open('resources/statuses.csv', newline=''))
+    print("Level setup started for: " + LEVEL_PATH)
+    create_tables()
+    print(f'Level creation complete for: {LEVEL_PATH}')
+    start_message = ('Helpful start message')
 
 def create_tables():
-    # Get the connection name of the db instance
     credentials, project_id = google.auth.default()
     service = discovery.build('sqladmin', 'v1beta4', credentials=credentials)
     response = service.instances().list(project=project_id).execute()
-    instance_name = response['items'][0]['connectionName']
+    connection_name = response['items'][0]['connectionName']
+    instance_name = response['items'][0]['name']
+    user = {'kind':'sql#user','name':'api-engine','project':project_id,'instance':instance_name,'password':'psw'}
+    service.users().insert(project=project_id, instance=instance_name, body=user).execute()
 
-    # Download a proxy manager to authenticate connection
-    subprocess.call('./auth_proxy.sh')
-    proxy = subprocess.Popen(['./cloud_sql_proxy', f'-instances={instance_name}=tcp:5432'])
+    proxy = subprocess.Popen(['./cloud_sql_proxy', f'-instances={connection_name}=tcp:5432'])
     time.sleep(5)
 
     db_config = {
@@ -99,22 +74,21 @@ def create_tables():
         "pool_recycle": 1800,  # 30 minutes
     }
 
-    pool = sqlalchemy.create_engine(
+    db = sqlalchemy.create_engine(
         sqlalchemy.engine.url.URL(
             drivername="postgresql+pg8000",
-            username="postgres",
+            username="api-engine",
             password="psw",
+            database="userdata-db",
             host='127.0.0.1',
-            port=5432,  # e.g. 5432
-            database='userdata-db'
+            port=5432
         ),
         **db_config
     )
+    db.dialect.description_encoding = None
 
-    pool.dialect.description_encoding = None
-    devs = csv.DictReader(open('resources/devs.csv', newline=''))
-
-    with db.connect(user_db_name) as conn:
+    devs = csv.DictReader(open('devs.csv', newline=''))
+    with db.connect() as conn:
         conn.execute(
             """CREATE TABLE users (
                 id       SERIAL,
@@ -131,11 +105,10 @@ def create_tables():
         )
 
         for dev in devs:
-            query = f"""INSERT INTO devs (name, phone, address)
-                        VALUES ({dev[name]}, {dev[phone]}, {dev[address]});
-                    """
-            conn.execute(query)
-
-    pool.dispose()
+            stmt = text("INSERT INTO devs (name, phone, address) VALUES (:name, :phone, :address)")
+            conn.execute(stmt, dev)
     proxy.terminate()
-    os.system(['rm', 'cloud_sql_proxy'])
+    db.dispose()
+
+def destroy():
+    deployments.delete()
